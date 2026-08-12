ed864af34c66eb0edb2523e797e483a18eeb4359
gtr3base <aloxaryabov@gmail.com>
2026-08-12 13:01:01 +0300

    init

---
+++ src/test/java/gtr3base/dev/hotel_api/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gtr3base.dev.hotel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
